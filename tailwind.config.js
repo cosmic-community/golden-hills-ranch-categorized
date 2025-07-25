31675029228110e6b969793dff129467428865e8
Cosmic AI <support@cosmicjs.com>
2025-07-25 16:15:52 -0700

    Change brand primary color from ranch-brown to cosmic blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'ranch-brown': '#228B22',
+        'ranch-brown': '#1E40AF',
         'ranch-gold': '#DAA520',
         'ranch-cream': '#F5F5DC',
         'cosmic-dark': '#11171A',
